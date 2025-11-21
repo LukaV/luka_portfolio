f3610bdd7937b0aa627bdb54f3ee3303415cd832
LukaV <luka.vukovic.2005@gmail.com>
2025-11-21 16:39:25 +0800

    Add basic frontpage with animated hero and title name

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,14 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import gsap from "gsap";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+/**
+ * Animates the name and hero image when the webpage loads.
+ */
+function introAnimation(): void {
+  gsap.from("#name-title", { duration: 2, y: '-100vh', ease: "back.inOut" });
+  gsap.from("#hero", { duration: 2.5, y: '100vh', ease: "expo.out" });
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+
+
+introAnimation();
