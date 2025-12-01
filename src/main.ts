8b08e2fa8146eade6d5112b6d13297a2453d9cba
LukaV <luka.vukovic.2005@gmail.com>
2025-12-01 12:28:42 +0800

    Change frontpage animation and add projects and experience section without project info

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,91 @@
+import './style.css'
 import gsap from "gsap";
+import ScrollTrigger from 'gsap/ScrollTrigger';
+import Lenis from 'lenis';
+
+// ScrollTrigger Plugin used with Lenis to sync animations with scrolling. 
+gsap.registerPlugin(ScrollTrigger)
 
 /**
- * Animates the name and hero image when the webpage loads.
+ * Animates the last name when the webpage loads.
+*/
+function introSection(): void {
+  gsap.from("#last-name", {
+    duration: 2, x: '100vw', ease: "power4.inOut"
+  });
+}
+
+/**
+ * Animates the project title and horizontally scrolls across each project.
  */
-function introAnimation(): void {
-  gsap.from("#name-title", { duration: 2, y: '-100vh', ease: "back.inOut" });
-  gsap.from("#hero", { duration: 2.5, y: '100vh', ease: "expo.out" });
+function projectExperienceSection(): void {
+  const section: HTMLElement = document.querySelector("#horizontal-project-exp-section") as HTMLElement;
+  const titleText: HTMLElement = document.querySelector("#projects-exp-title") as HTMLElement;
+  const projectsWrapper: HTMLElement = document.querySelector("#projects-wrapper") as HTMLElement;
+
+  // Create the animation timeline.
+  let timeline: gsap.core.Timeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: section,
+      pin: true,
+      start: 'top top', // start the animation when the trigger hits the top of the viewport.
+      end: `+=${section.scrollWidth}`,
+      scrub: 1,
+      invalidateOnRefresh: true,
+      snap: {
+        snapTo: 'labels',
+        duration: { min: 0.2, max: 3 },
+        delay: 0.2, 
+        ease: 'sine.inOut'
+      }
+    }
+  });
+
+  // Get each project's html element.
+  const project1: HTMLElement = document.querySelector("#project-1") as HTMLElement;
+  const project2: HTMLElement = document.querySelector("#project-2") as HTMLElement;
+  const project3: HTMLElement = document.querySelector("#project-3") as HTMLElement;
+
+  // Get each project's x-axis location.
+  const project1XAxis = project1.getBoundingClientRect().x;
+  const project2XAxis = project2.getBoundingClientRect().x;
+  const project3XAxis = project3.getBoundingClientRect().x;
+
+  const titleEase: string = "sine.inOut"
+  const projectEase: string = "none"
+  timeline.addLabel("start")
+    // Title.
+    .from(".ampersand", {
+      x: "100vw", ease: titleEase
+    })
+    .from("#project-word", {
+      x: "-100vw", y: "-100vh", ease: titleEase
+    })
+    .from("#exp-word", {
+      x: "100vw", y: "100vh", ease: titleEase
+    }, "<")
+
+    // Projects. Moves the wrapper to each project's x-axis.
+    .addLabel("project1")
+    .to(projectsWrapper, { x: -project1XAxis, ease: projectEase})
+    .addLabel("project2")
+    .to(projectsWrapper, { x: -project2XAxis, ease: projectEase})
+    .addLabel("project3")
+    .to(projectsWrapper, { x: -project3XAxis, ease: projectEase})
+
+    // Title and last project fade out at the same time.
+    .addLabel("end")
+    .to(titleText, { opacity: 0, ease: "none"})
+    .to(project3, { opacity: 0, ease: "none"}, "<")
 }
 
 
+// Initialise Lenis.
+const lenis = new Lenis({autoRaf: true});
 
+// Connect GSAP ScrollTrigger to sync animations with scrolling.
+lenis.on('scroll', ScrollTrigger.update);
 
-introAnimation();
+// Call functions.
+introSection();
+projectExperienceSection();
